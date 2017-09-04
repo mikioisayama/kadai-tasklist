800082fa4fc896309c460c88e0bdd78df1482279
mikio.isayama <isa.s.w85@gmail.com>
2017-09-04 12:44:08 +0000

    修正2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,11 +50,11 @@ class TasksController < ApplicationController
   
   private
   
-  def set_message
+  def set_task
     @task = Task.find(params[:id])
   end
   
   def task_params
-    params.require(:task).permit(:content, :title)
+    params.require(:task).permit(:content, :title, :status)
   end
 end
