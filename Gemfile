8b6d5dad79d529a6dddbe7a70e095e842f83b019
mikio.isayama <isa.s.w85@gmail.com>
2017-09-11 14:18:48 +0000

    tasklist-tweet1

--- Gemfile
+++ Gemfile
@@ -53,8 +53,8 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 group :production do
-  gem 'pg'
-  
+ gem 'pg'
+end
+
 gem 'kaminari'
 
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
+ 
   def index
-     @users = User.all.page(params[:page])
+    @users = User.all.page(params[:page])
   end
 
   def show
